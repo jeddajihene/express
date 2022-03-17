b9829b1244e4ddb36cab6db52d0adbf56ba41a78
jedda jihene <jeddajihene1@gmail.com>
2022-03-17 22:44:09 +0100

    cheekpiont ex

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
-//..............................
 const { dirname } = require("path");
 const app = express();
 const PORT = 5000;
+// ........................................
+app.use(express.static(__dirname + "/views"));
+// .........................................
 const path = require("path");
 app.listen(PORT, () => console.log(`server is  runing  ${PORT}`));
 
@@ -11,21 +12,20 @@ const middlware = (req, res, next) => {
   const date = new Date();
   if (
     date.getDay() > 0 &&
-    date.getDay <= 5 &&
+    date.getDay() <= 5 &&
     date.getHours() >= 9 &&
     date.getHours() <= 17
   ) {
-    return res.sendFile(path.join(__dirname, "views", "welcome.png"));
+    next();
   }
-
-  next();
-
-  app.use(middlware);
+  res.sendFile(path.join(__dirname, "views", "Closed.html"));
 };
+app.use(middlware);
+
 // closed
 
 app.get("/closed", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "closed.html"));
+  res.sendFile(path.join(__dirname, "views", "Closed.html"));
 });
 app.use(express.static("views"));
 // HOME
@@ -35,12 +35,12 @@ app.get("/", (req, res) => {
 app.use(express.static("views"));
 
 // contact
-app.get("/Contact", (req, res) => {
+app.get("/contact", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "Contact.html"));
 });
 app.use(express.static("views"));
 // service
-app.get("/service", (req, res) => {
+app.get("/services", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "service.html"));
 });
 app.use(express.static("views"));
